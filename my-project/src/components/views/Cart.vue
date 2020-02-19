3ebf65b543212081d8c5bda9567f2203d5a98c50
fahad <fahadbinmnr@gmail.com>
2020-02-19 12:44:33 +0600

    added an extra line in brabnchB

--- my-project/src/components/views/Cart.vue
+++ my-project/src/components/views/Cart.vue
@@ -5,13 +5,20 @@
 			<h5 class="card-title">Card title</h5>
 			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 			<a href="#" class="btn btn-primary">Go somewhere</a>
+			<h1>Hello,this is {{ name }} </h1>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
 
+		return {
+
+			name: 'Fahad Bin Munir'
+		}
+	}
 }
 </script>
 
